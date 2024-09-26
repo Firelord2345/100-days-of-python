b318c06bc9119a01215ae9d90c62e25709fc7959
Joel <joelroyskroys@gmail.com>
2024-09-26 13:38:48 +0530

    Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,29 @@
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+# student_grades ={}
+# for grade in student_scores:
+#     if student_scores[grade]>90:
+#         student_grades[grade]="Outstanding"
+#     elif student_scores[grade]>80 and student_scores[grade]<=90:
+#         student_grades[grade]="Exceeds Expectations"
+#     elif student_scores[grade]>70 and student_scores[grade]<=80:
+#         student_grades[grade]="Acceptable"
+#     elif student_scores[grade]<=70 :
+#         student_grades[grade]="Fail"
+
+# print(student_grades)
+# we can empty an dictionary
+# student_grades={}
+# print(student_grades)
+# we rename the string
+# student_scores['Harry']=81
+# print(student_scores)
+# we add the string
+student_scores['BOB']=81
+print(student_scores)
+
