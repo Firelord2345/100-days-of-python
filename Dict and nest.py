682831ad30a788952f880ae22a7083d7ecc37562
Joel <joelroyskroys@gmail.com>
2024-09-26 14:03:48 +0530

    Dict and Nest

---
+++ Dict and nest.py
@@ -0,0 +1,9 @@
+# student_scores = {
+#     'Haray':["hi",'hello',{'kio':{1:1,2:2}}],
+#     'Ron': 78,
+#     'Hermione': 95,
+#     'Draco': 75,
+#     'Neville': 60
+# }
+# print(student_scores['Haray'][2]['kio'][2])
+# here the dict consist of list inside a list contains dict
